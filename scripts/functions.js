cfa17f3348664a90ca6c8d3b9ad97717af2f735b
JavierGuijarro <javierguijarroverdu@gmail.com>
2023-09-22 19:53:50 +0200

    Ejercicio 1.2 frase - DWEC

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,46 @@
 'use strict'
 
 function letters(cadena) {
-
+	return cadena.length
 }
 
 function words(cadena) {
-
+	let cadenaEspaciada = cadena.split(" ")
+	return cadenaEspaciada.length
 }
 
 function upperString(cadena) {
-
+	return cadena.toUpperCase()
 }
 
 function titleString(cadena) {
+	let cadenaEspaciada = cadena.split(" ")
+	let cadenaMayusculas = []
+	for (let valor of cadenaEspaciada) {
+		valor.charAt(0).toUpperCase()
+		cadenaMayusculas.push(valor.charAt(0).toUpperCase() + valor.slice(1)
+		)
+	}
+	return cadenaMayusculas.join(" ");
 
 }
 
 function backwardsLetters(cadena) {
-
+	let cadenaEspaciada = cadena.split("")
+	let cadenaReversa = cadenaEspaciada.reverse()
+	return cadenaReversa.join("")
 }
 
 function backwardsWords(cadena) {
-
+	let cadenaEspaciada = cadena.split(" ");
+	let cadenaReversa = cadenaEspaciada.reverse();
+	return cadenaReversa.join(" ");
 }
 
 function palindrome(cadena) {
-
+	cadena = cadena.toLowerCase().replaceAll(" ", "")
+	let cadenaReversa = backwardsLetters(cadena)
+	return cadena === cadenaReversa
 }
 
 module.exports = {
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,17 @@
 'use strict'
 
+let cadena = prompt("Introduce una frase:")
 
+if (cadena == null) {
+    alert("Has cancelado el ejercicio")
+} else if (cadena.trim() === "") {
+    alert("Debes rellenar el formulario")
+} else {
+    console.log("Frase: " + cadena)
+    console.log(letters(cadena) + " letras y " + words(cadena) + " palabras")
+    console.log(upperString(cadena))
+    console.log(titleString(cadena))
+    console.log(backwardsLetters(cadena))
+    console.log(backwardsWords(cadena))
+    console.log(palindrome(cadena)?"Si es un palíndromo":"No es un palíndromo")
+}
